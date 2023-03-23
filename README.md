df03d7fea215e29486b0794826dba2047bcb8419
AnonSubmissions123 <128703254+AnonSubmissions123@users.noreply.github.com>
2023-03-23 09:20:45 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Unity Demo
+The goal of this demo is to show how  [LibEMG]() can be leveraged from a different tech stack (in this case Unity). Please check out the the [Example 2]() from the docs for a full walkthrough.
+
+![momo](Docs/Momo_Myo.gif)
---
+++ myo_control.py
@@ -0,0 +1,101 @@
+import socket
+import multiprocessing
+from tkinter import *
+from libemg.screen_guided_training import ScreenGuidedTraining
+from libemg.data_handler import OnlineDataHandler, OfflineDataHandler
+from libemg.utils import make_regex
+from libemg.feature_extractor import FeatureExtractor
+from libemg.emg_classifier import OnlineEMGClassifier, EMGClassifier 
+from libemg.streamers import myo_streamer
+
+class Menu:
+    def __init__(self):
+        # Myo Streamer - start streaming the myo data 
+        myo_streamer()
+
+        # Create online data handler to listen for the data
+        self.odh = OnlineDataHandler()
+        self.odh.start_listening()
+
+        self.classifier = None
+
+        self.window = None
+        self.initialize_ui()
+        self.window.mainloop()
+
+    def initialize_ui(self):
+        # Create the simple menu UI:
+        self.window = Tk()
+        self.window.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.window.title("Game Menu")
+        self.window.geometry("500x200")
+
+        # Label 
+        Label(self.window, font=("Arial bold", 20), text = 'LibEMG - Unity Demo').pack(pady=(10,20))
+        # Train Model Button
+        Button(self.window, font=("Arial", 18), text = 'Train Model', command=self.launch_training).pack(pady=(0,20))
+        # Play Snake Button
+        Button(self.window, font=("Arial", 18), text = 'Start Classifying', command=self.start_classifying).pack()
+
+    def start_classifying(self):
+        self.window.destroy()
+        self.start_classifying()
+
+    def launch_training(self):
+        self.window.destroy()
+        training_ui = ScreenGuidedTraining()
+        training_ui.download_gestures([1,2,4,5], "images/")
+        training_ui.launch_training(self.odh, 2, 3, "images/", "data/", 1)
+        self.initialize_ui()
+
+    def start_classifying(self):
+        WINDOW_SIZE = 50 
+        WINDOW_INCREMENT = 10
+
+        # Step 1: Parse offline training data
+        dataset_folder = 'data/'
+        classes_values = ["0","1","2","3"]
+        classes_regex = make_regex(left_bound = "_C_", right_bound=".csv", values = classes_values)
+        reps_values = ["0", "1", "2"]
+        reps_regex = make_regex(left_bound = "R_", right_bound="_C_", values = reps_values)
+        dic = {
+            "reps": reps_values,
+            "reps_regex": reps_regex,
+            "classes": classes_values,
+            "classes_regex": classes_regex
+        }
+
+        odh = OfflineDataHandler()
+        odh.get_data(folder_location=dataset_folder, filename_dic=dic, delimiter=",")
+        train_windows, train_metadata = odh.parse_windows(WINDOW_SIZE, WINDOW_INCREMENT)
+
+        # Step 2: Extract features from offline data
+        fe = FeatureExtractor()
+        feature_list = fe.get_feature_groups()['LS9']
+        training_features = fe.extract_features(feature_list, train_windows)
+
+        # Step 3: Dataset creation
+        data_set = {}
+        data_set['training_features'] = training_features
+        data_set['training_labels'] = train_metadata['classes']
+
+        # Step 4: Create the EMG classifier
+        o_classifier = EMGClassifier()
+        o_classifier.fit(model="SVM", feature_dictionary=data_set)
+        o_classifier.add_velocity(train_windows, train_metadata['classes'])
+        o_classifier.add_majority_vote(5)
+        o_classifier.add_rejection(0.9)
+
+        # Step 5: Create online EMG classifier and start classifying.
+        self.classifier = OnlineEMGClassifier(o_classifier, WINDOW_SIZE, WINDOW_INCREMENT, self.odh, feature_list)
+        self.classifier.run(block=False) # block set to false so it will run in a seperate process.
+
+    def on_closing(self):
+        # Clean up all the processes that have been started
+        if not self.classifier is None:
+            self.classifier.stop_running()
+        self.odh.stop_listening()
+        self.window.destroy()
+
+if __name__ == "__main__":
+    menu = Menu()
